7acdf43a5763f63842bba4d61c68df698022a659
artygrand <2451297+artygrand@users.noreply.github.com>
2025-10-30 05:10:49 +0000

    Fix styling

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,9 +3,9 @@
 use Symfony\Component\Finder\Finder;
 
 it('does not use dd, dump, or ray in package files', function () {
-    $finder = new Finder();
+    $finder = new Finder;
     $finder->files()
-        ->in([__DIR__ . '/../src', __DIR__])
+        ->in([__DIR__.'/../src', __DIR__])
         ->name('*.php');
 
     $pattern = '/\b(?:dd|dump|ray)\s*\(/i';
