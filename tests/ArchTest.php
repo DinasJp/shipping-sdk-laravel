8667c9eb091852b1894bd1d7c89ec167bfdf66bb
Arthur Grand <artygrand@gmail.com>
2025-10-30 14:10:29 +0900

    Update ArchTest.php

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,22 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+use Symfony\Component\Finder\Finder;
+
+it('does not use dd, dump, or ray in package files', function () {
+    $finder = new Finder();
+    $finder->files()
+        ->in([__DIR__ . '/../src', __DIR__])
+        ->name('*.php');
+
+    $pattern = '/\b(?:dd|dump|ray)\s*\(/i';
+
+    foreach ($finder as $file) {
+        $contents = $file->getContents();
+        if (preg_match($pattern, $contents, $m)) {
+            $path = $file->getRealPath();
+            $this->fail("Found debugging function \"{$m[0]}\" in {$path}");
+        }
+    }
+
+    $this->assertTrue(true);
+});
